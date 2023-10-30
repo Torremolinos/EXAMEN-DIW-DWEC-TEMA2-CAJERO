cd0a2344208e124c409e54dbb0e2c25a4c60842f
Torremolinos <adrian_2054@hotmail.com>
2023-10-30 08:54:34 +0100

    Añado las variables, el DOM y empiezo con los metodos de JS

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+/**
+ * @Author: Adrian Iglesias Riño
+ * @github: https//github.com/
+ */
+
+document.addEventListener('DOMContentLoaded',() =>{
+
+    //Declaro las variables globales que voy a usar.
+    let saldo = 1000;
+    const PIN= "1234";
+    let intentos = 3;
+
+    //Enlazo el html con el js usando DOM es decir los id
+
+    const depositar = document.getElementById("depositar");
+    const retirar = document.getElementById("retirar");
+    const transferir = document.getElementById("transferir");
+    const cambiarContrasenia = document.getElementById("cambiar");
+    const salir = document.getElementById("salir");
+    const spanSaldo = document.getElementById("resultado");
+
+    // hago los metodos.
+
+    //empiezo con leer el saldo.
+    const saldoActual = () =>{
+
+    }
+});
